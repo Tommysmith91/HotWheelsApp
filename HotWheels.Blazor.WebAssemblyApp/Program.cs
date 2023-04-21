87abed3b4f09ba0b5eb942489d7ac5b9132b7abb
tommy.smith <tommy.smith@cloudcommercegroup.com>
2023-04-21 14:03:34 +0100

    Added google app auth for blazor wasm

--- HotWheels.Blazor.WebAssemblyApp/Program.cs
+++ HotWheels.Blazor.WebAssemblyApp/Program.cs
@@ -2,19 +2,26 @@ using HotWheels.Blazor.WebAssemblyApp;
 using HotWheelsApp.HttpClients;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Net.Http.Headers;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
 builder.Services.AddHttpClient<IHotWheelsClient, HotWheelsClient>(client =>
 {
     client.BaseAddress = new Uri("http://localhost:5073/");
 });
 
 
-    
+
+
 
 await builder.Build().RunAsync();
---
+++ HotWheels.Blazor.WebAssemblyApp/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+
+namespace HotWheels.Blazor.WebAssemblyApp.Shared
+{
+    public partial class MainLayout
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; } = null;
+
+        [CascadingParameter]
+        protected Task<AuthenticationState> AuthStat { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            base.OnInitialized();
+            var user = (await AuthStat).User;
+            if (!user.Identity.IsAuthenticated)
+            {
+                NavigationManager.NavigateTo($"authentication/login?returnUrl={Uri.EscapeDataString(NavigationManager.Uri)}");
+            }
+        }
+    }
+}
+
